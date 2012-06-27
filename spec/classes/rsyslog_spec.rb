77a82b38e39342311371277f61843debe196752d
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-06-27 13:58:43 +0200

    Package set to present instead of latest, due to conflicts between rsyslog and syslog-ng

--- spec/classes/rsyslog_spec.rb
+++ spec/classes/rsyslog_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe 'rsyslog' do
   it { should contain_class 'rsyslog' }
   it { should contain_service('rsyslog').with_ensure('running') }
-  it { should contain_package('rsyslog').with_ensure('latest') }
+  it { should contain_package('rsyslog').with_ensure('present') }
 	it { should contain_file('/etc/rsyslog.conf').with({
         'ensure' => 'file',
         'owner'  => 'root',
