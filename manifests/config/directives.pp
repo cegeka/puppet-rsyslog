b558861f799ad53a63027ab245db041da8d37e37
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-11-22 12:23:52 +0100

    Added define for configuration directives

---
+++ manifests/config/directives.pp
@@ -0,0 +1,21 @@
+define rsyslog::config::directives (
+  $content = []
+)
+{
+  case $::osfamily {
+    'RedHat': {
+      file { "/etc/rsyslog.d/${title}.conf":
+        ensure   => 'file',
+        owner    => 'root',
+        group    => 'root',
+        mode     => '0644',
+        content  => template("${module_name}/config/directives.erb"),
+        notify   => 'Service[rsyslog]',
+        require  => 'File[/etc/rsyslog.d]'
+      }
+    }
+    default: {
+      fail("Rsyslog::Configuration[${title}]: osfamily ${::osfamily} is not supported")
+    }
+  }
+}
